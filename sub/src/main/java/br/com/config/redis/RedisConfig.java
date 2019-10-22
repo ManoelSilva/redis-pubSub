f45af30ef3e5d74767b1b2f4a2aa9d36c9945197
Manoel Silva <manoelps.ti@gmail.com>
2019-10-21 23:19:40 -0300

    Resources and other configs adjusts

--- sub/src/main/java/br/com/config/redis/RedisConfig.java
+++ sub/src/main/java/br/com/config/redis/RedisConfig.java
@@ -3,10 +3,12 @@ package br.com.config.redis;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
+@Configuration
 public class RedisConfig {
 	private final RedisClusterProperties clusterProperties;
 
@@ -36,7 +38,7 @@ public class RedisConfig {
 		template.setConnectionFactory(jedisConnectionFactory());
 		return template;
 	}
-
+	
 	@Bean
 	public RedisTemplate<String, Integer> redisTemplateLong() {
 		RedisTemplate<String, Integer> template = new RedisTemplate<>();
--- sub/src/main/java/br/com/config/task/SubscriberTask.java
+++ sub/src/main/java/br/com/config/task/SubscriberTask.java
@@ -4,7 +4,6 @@ import java.util.TimerTask;
 import java.util.concurrent.TimeUnit;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
@@ -15,22 +14,18 @@ public class SubscriberTask extends TimerTask {
 	private static final String SUB_CHANNEL = "sub-channel";
 
 	private RedisTemplate<String, String> redisTemplate;
-	private ListOperations<String, String> operations;
 	private SubService service;
 
 	@Autowired
-	public SubscriberTask(RedisTemplate<String, String> redisTemplate, ListOperations<String, String> operations,
-			SubService service) {
+	public SubscriberTask(RedisTemplate<String, String> redisTemplate, SubService service) {
 		this.redisTemplate = redisTemplate;
-		this.operations = operations;
 		this.service = service;
 	}
 
 	@Override
 	public void run() {
-		operations = redisTemplate.opsForList();
 		while (true) {
-			String json = operations.rightPop(SUB_CHANNEL, 0, TimeUnit.NANOSECONDS);
+			String json = redisTemplate.opsForList().rightPop(SUB_CHANNEL, 0, TimeUnit.NANOSECONDS);
 			this.service.buildMessages(json);
 		}
 	}
